51cfd5da4e133af7a7df183215148f2e06f20bb1
tsekot sergey <tsekot.sergey@gmail.com>
2024-03-30 08:25:49 +0100

    1

---
+++ core/src/com/me/mariobros/MarioBros.java
@@ -0,0 +1,28 @@
+package com.me.mariobros;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.me.mariobros.screens.PlayScreen;
+
+public class MarioBros extends Game {
+    public static final int V_WIDTH = 400;
+    public static final int V_HEIGHT = 208;
+    // pixels per meter
+    public static final float PPM = 100;
+    public SpriteBatch batch;
+
+    @Override
+    public void create() {
+        batch = new SpriteBatch();
+        setScreen(new PlayScreen(this));
+    }
+
+    @Override
+    public void render() {
+        super.render();
+    }
+
+    @Override
+    public void dispose() {
+    }
+}
---
+++ core/src/com/me/mariobros/screens/PlayScreen.java
@@ -0,0 +1,171 @@
+package com.me.mariobros.screens;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.maps.MapObject;
+import com.badlogic.gdx.maps.objects.RectangleMapObject;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
+import com.badlogic.gdx.physics.box2d.PolygonShape;
+import com.badlogic.gdx.physics.box2d.World;
+import com.badlogic.gdx.utils.viewport.FillViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
+import com.me.mariobros.MarioBros;
+import com.me.mariobros.scenes.Hud;
+import com.me.mariobros.sprite.Mario;
+import com.me.mariobros.tools.B2WorldCreator;
+
+public class PlayScreen implements Screen {
+
+    private MarioBros game;
+    private TextureAtlas atlas;
+
+    private OrthographicCamera gamecam;
+    private Viewport gamePort;
+
+    private Hud hud;
+    private TmxMapLoader mapLoader;
+    private TiledMap map;
+    private OrthogonalTiledMapRenderer renderer;
+
+    private World world;
+    private Box2DDebugRenderer b2dr;
+    private Mario player;
+
+    public PlayScreen(MarioBros game) {
+
+
+        this.atlas = new TextureAtlas("mario_and_enemies.pack");
+        this.game = game;
+
+        gamecam = new OrthographicCamera();
+        gamePort = new FillViewport(MarioBros.V_WIDTH / MarioBros.PPM, MarioBros.V_HEIGHT / MarioBros.PPM, gamecam);
+        hud = new Hud(game.batch);
+
+        mapLoader = new TmxMapLoader();
+        map = mapLoader.load("level1.tmx");
+        renderer = new OrthogonalTiledMapRenderer(map, 1 / MarioBros.PPM);
+        gamecam.position.set(gamePort.getWorldWidth() / 2, gamePort.getWorldHeight() / 2, 0);
+
+        world = new World(new Vector2(0, -10), true);
+        b2dr = new Box2DDebugRenderer();
+
+        new B2WorldCreator(world, map);
+
+        player = new Mario(world, this);
+    }
+
+    public TextureAtlas getAtlas() {
+        return atlas;
+    }
+
+    public void update(float dt) {
+        handleInput(dt);
+
+        world.step(1 / 60f, 6, 2);
+
+        player.update(dt);
+
+        // attach our gamecam to our players.x coordinate
+        gamecam.position.x = player.b2body.getPosition().x;
+
+        gamecam.update();
+
+        renderer.setView(gamecam);
+    }
+
+    private void handleInput(float dt) {
+        if (Gdx.input.isKeyJustPressed(Input.Keys.UP)) {
+            player.b2body.applyLinearImpulse(new Vector2(0, 4f), player.b2body.getWorldCenter(), true);
+        }
+
+        if (Gdx.input.isTouched()) {
+            if (Gdx.input.getX() < Gdx.graphics.getWidth() / 2 && player.b2body.getLinearVelocity().x >= -2) {
+                //left
+                player.b2body.applyLinearImpulse(new Vector2(-0.1f, 0), player.b2body.getWorldCenter(), true);
+            } else if (player.b2body.getLinearVelocity().x <= 2) {
+                player.b2body.applyLinearImpulse(new Vector2(0.1f, 0), player.b2body.getWorldCenter(), true);
+            }
+
+            if (Gdx.input.getY() < Gdx.graphics.getHeight() / 2) {
+                player.b2body.applyLinearImpulse(new Vector2(0, 0.4f), player.b2body.getWorldCenter(), true);
+            }
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.RIGHT) && player.b2body.getLinearVelocity().x <= 2) {
+            player.b2body.applyLinearImpulse(new Vector2(0.1f, 0), player.b2body.getWorldCenter(), true);
+        }
+
+        if (Gdx.input.isKeyPressed(Input.Keys.LEFT) && player.b2body.getLinearVelocity().x >= -2) {
+            player.b2body.applyLinearImpulse(new Vector2(-0.1f, 0), player.b2body.getWorldCenter(), true);
+        }
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+
+        update(delta);
+
+        Gdx.gl.glClearColor(0, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        // render our game map
+        renderer.render();
+
+        // render our Box2DebugLines
+        b2dr.render(world, gamecam.combined);
+
+        game.batch.setProjectionMatrix(gamecam.combined);
+        game.batch.begin();
+        player.draw(game.batch);
+        game.batch.end();
+
+        // set our batch to now draw what the hud camera sees
+        game.batch.setProjectionMatrix(hud.stage.getCamera().combined);
+        hud.stage.draw();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        gamePort.update(width, height);
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+        map.dispose();
+        renderer.dispose();
+        world.dispose();
+        b2dr.dispose();
+        hud.dispose();
+    }
+}
